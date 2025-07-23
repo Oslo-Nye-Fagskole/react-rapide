15f608fdd7c7efbe6a401b75ddad804ffc9e6f2c
Ironboy <ironboy@users.noreply.github.com>
2025-07-23 02:17:13 +0200

    done

--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,15 @@ export default function App() {
     <h1>Data for Your Wilde Frames Assignment</h1>
     <p>You will find the following in the <b>public</b> folder</p>
     <ul>
-      <li>Images of all the animals.</li>
-      <li>The necessary JSON data.</li>
-      <li>A <a href="/css-example-display-frames/index.html">CSS-example of how to display frames, with mats</a>. Feel free to use the basic CSS structure, but integrate it with your own programming logic: <b>Do not</b> copy and modify the CSS for each frame size.</li>
-      <li>A <a href="/product-detail-fast-mockup.html">mockup of a animal/product detail page</a>, for inspiration/viewing purposes only. Do <b>not</b> use the HTML/CSS structure from this example, it is not React-friendly.</li>
+      <li>A <a href="/examples/product-detail-fast-mockup.html">mockup of an animal/product detail page</a>, for inspiration/viewing purposes only.<br /><b>Do not</b> use the HTML/CSS structure from this example, it is not React-friendly.</li>
+      <li>A <a href="/examples/css-example-display-frames/index.html">CSS-example of how to display frames, with mats</a>. Feel free to use the basic CSS structure, but integrate it with your own programming logic:<br /><b>Do not</b> copy and modify the CSS for each frame size.</li>
+      <li>Images of all the animals (in the folder <b>animal-images</b>).</li>
+      <li>JSON data you can use for early prototyping of the shop (in the folder <b>json</b>).</li>
     </ul>
-    <hr />
-    <h2>README FOR REZA:</h2>
-    <pre>
-      Hi Reza!<br />
-      <br />
-      TO DO:<br />
-      * This should install a little differenlty than other examples, not replacing the source folder or public folder entirely... rather just giving you the files you need.<br />
-      * Integration of the data (currently JSON files only) into a backend similar to the one in our REST-api / shopping cart examples to be used in latter stages of the assignment.
-      <br /><br />
-      /Thomas
-    </pre>
+    <h1>A Complete REST-api for the Assignment</h1>
+    <p>You can use the code you have installed right now (this <b>react rapide</b>-example) as the base for your own repo, it contains a backend with a fully working REST-api for the assignment:</p>
+    <ul>
+      <li><a href="/examples/rest-api-documentation/index.html">REST-api-documentation</a></li>
+    </ul>
   </>;
 }
